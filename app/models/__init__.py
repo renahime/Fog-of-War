f91ab0d9099dc6317432a3d30f981751b2610ec1
renahime <genuinehex@gmail.com>
2023-06-14 09:48:57 -0700

    starting routes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .threads import Thread
+from .category import Category
+from .posts import Post
---
+++ app/seeds/category.py
@@ -0,0 +1,54 @@
+from app.models import db, Category, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_users():
+    movies = Category(
+        name='Movies')
+    literature = Category(
+        name='Literature')
+    anime_manga = Category(
+        name='Anime, Manga, Visual Novels')
+    videogames = Category(
+        name='Video Games')
+    tv_shows = Category(
+        name='TV Shows')
+
+    off_topic_media = Category(
+        name='Off Topic Media')
+    history = Category(
+        name='History')
+    religion_philosophy = Category(
+        name='Religion/Philosophy')
+    food = Category(
+        name='Food')
+    pet = Category(
+        name='Pets')
+
+    db.session.add(movies)
+    db.session.add(literature)
+    db.session.add(anime_manga)
+    db.session.add(videogames)
+    db.session.add(tv_shows)
+    db.session.add(off_topic_media)
+    db.session.add(history)
+    db.session.add(religion_philosophy)
+    db.session.add(food)
+    db.session.add(pet)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_users():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM users"))
+
+    db.session.commit()
