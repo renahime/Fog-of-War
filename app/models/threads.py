7f8909af9dc7d73d4ad46417a4873d979169a213
renahime <genuinehex@gmail.com>
2023-06-14 08:41:51 -0700

    thread model made

---
+++ app/models/threads.py
@@ -0,0 +1,39 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime, timedelta
+
+
+class Thread(db.Model):
+    __tablename__ = 'threads'
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    views = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    category = db.relationship('Category', back_populates='threads', passive_deletes=True)
+    user = db.relationship('User', back_populates='threads')
+    posts = db.relationship('Post', back_populates='thread', ondelete='CASCADE')
+
+    def to_dict_with_txt(self):
+        return{
+            'id': self.id,
+            'subject': self.subject,
+            'views': self.views,
+            'text': self.text,
+            'user': self.user.to_dict(),
+            'category': self.category.to_dict(),
+            'posts': self.posts.to_dict()
+        }
+
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'subject': self.subject,
+            'views': self.views,
+            'user': self.user.to_dict(),
+            'category': self.category.to_dict(),
+            'post_count': len(self.posts.to_dict())
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    signature = db.Column(db.Text)
+    about = db.Column(db.Text)
 
     @property
     def password(self):
