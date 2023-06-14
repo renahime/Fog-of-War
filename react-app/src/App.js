7463e63bf272be4b211bc4910284756eae3cd66f
renahime <genuinehex@gmail.com>
2023-06-14 10:45:58 -0700

    starting landing page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -24,6 +24,8 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
+          <Route path="/">
+          </Route>
         </Switch>
       )}
     </>
