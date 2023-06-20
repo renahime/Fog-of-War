51fcc03f840f20d3b156dbde915d5e3935857c1e
renahime <genuinehex@gmail.com>
2023-06-20 14:27:37 -0700

    oops on main hehe

--- app/seeds/anime_posts.py
+++ app/seeds/anime_posts.py
@@ -14,19 +14,7 @@ def seed_posts():
 
     post1= Post(
         subject='RE: Umineko - My theory about Jessicas true origins + detailed analysis',
-        text="""Krauss probably does have some fertility issues given how Natsuhi got herself checked without any doctors seeing any issues with her but that doesn’t mean he’d have to be completely sterile.
-
-              “Natsuhi oba-san is pure and faithful.” - red truthed in the episode 5 ??? Natsuhi also mentions she’s never been with anyone other than her husband and at no point is this contradicted. Red truths can be tricky but I don’t think there’s a satisfactory way to weasel out of this one.
-
-              Married couples having separate beds is actually relatively common in Japan, especially for older couples.
-
-              Going to need some context for the “filthy” quote. Full sentence and when was this?
-
-              Natsuhi supposedly being kind to male servants was explicitly called a rumor rather than fact and we know at the very least it didn’t apply to Kanon. Sounds more like malicious, probably baseless gossip about a bad female boss than anything else.
-
-              Kinzo being the father would make Jessica and Kanon half-siblings. If they were that closely blood-related that probably would have come up more directly as an issue. Also it’s really not in keeping with the sense of wrongness we’re clearly supposed to view Erika’s theory with. “Oh, she’s not sleeping with her father-in-law ANYMORE, how dare you!” doesn’t have much bite to it. In general I don’t think Erika cared much about finding the truth so much as using things she can construct as being “the truth” in order to bully people.
-
-
+        text="""<p>Krauss probably does have some fertility issues given how Natsuhi got herself checked without any doctors seeing any issues with her but that doesn’t mean he’d have to be completely sterile. “Natsuhi oba-san is pure and faithful.” - red truthed in the episode 5 ??? Natsuhi also mentions she’s never been with anyone other than her husband and at no point is this contradicted. Red truths can be tricky but I don’t think there’s a satisfactory way to weasel out of this one. Married couples having separate beds is actually relatively common in Japan, especially for older couples. Going to need some context for the “filthy” quote. Full sentence and when was this? Natsuhi supposedly being kind to male servants was explicitly called a rumor rather than fact and we know at the very least it didn’t apply to Kanon. Sounds more like malicious, probably baseless gossip about a bad female boss than anything else. Kinzo being the father would make Jessica and Kanon half-siblings. If they were that closely blood-related that probably would have come up more directly as an issue. Also it’s really not in keeping with the sense of wrongness we’re clearly supposed to view Erika’s theory with. “Oh, she’s not sleeping with her father-in-law ANYMORE, how dare you!” doesn’t have much bite to it. In general I don’t think Erika cared much about finding the truth so much as using things she can construct as being “the truth” in order to bully people.</p><p>&nbsp;</p><p><br>&nbsp;</p>
               """,
         user=user2,
         thread=thread1
@@ -35,7 +23,7 @@ def seed_posts():
 
     post2 = Post(
         subject='RE: Umineko - My theory about Jessicas true origins + detailed analysis',
-        text="""If Jessica was Kinzo's child, then there would be no reason for the epitaph to come out as she would have defaulted into the headship anyways. Also the plot doesn't validate that Krauss was sterile, and in reality it's pretty normal for fertile couples to struggle or take time conceiving children. In this case there were perverse psychosomatic issues affecting both Klauss and Natsuhi too. Also there's the issue someone else mentioned that if Jessica was a consequence of Lion falling off the cliff, then there is no reason for her to exist in game 7 because Lion lived.""",
+        text="""<p>If Jessica was Kinzo's child, then there would be no reason for the epitaph to come out as she would have defaulted into the headship anyways. Also the plot doesn't validate that Krauss was sterile, and in reality it's pretty normal for fertile couples to struggle or take time conceiving children. In this case there were perverse psychosomatic issues affecting both Klauss and Natsuhi too. Also there's the issue someone else mentioned that if Jessica was a consequence of Lion falling off the cliff, then there is no reason for her to exist in game 7 because Lion lived.</p><p>&nbsp;</p><p><br>&nbsp;</p>""",
         user=user3,
         thread=thread1
     )
@@ -43,7 +31,7 @@ def seed_posts():
 
     post3= Post(
         subject="RE: Umineko - Just Finished Episode 2 This Morning, Here Are My Thoughts So Far",
-        text="""Great to hear that you're enjoying the vn. I had similar problems when I first started reading it, I thought the beginning was kinda slow so I ended up stopping like an hour or 2 in cause I just lost interest. Then a couple months later, I just decided to finish the 1st episode on a whim and it was the best decision I've made. Umineko is my fav visual novel now after reading it and I hope you enjoy your time with it too.""",
+        text="""<p>Great to hear that you're enjoying the vn. I had similar problems when I first started reading it, I thought the beginning was kinda slow so I ended up stopping like an hour or 2 in cause I just lost interest. Then a couple months later, I just decided to finish the 1st episode on a whim and it was the best decision I've made. Umineko is my fav visual novel now after reading it and I hope you enjoy your time with it too.</p><p>&nbsp;</p><p><br>&nbsp;</p>""",
         user=user2,
         thread=thread2
     )
@@ -52,8 +40,7 @@ def seed_posts():
 
     post4= Post(
         subject="RE: Umineko - Just Finished Episode 2 This Morning, Here Are My Thoughts So Far",
-        text="""yay! love reading people's opinions as they go through :) i think episode 2 is the one that really hooks you in and holds on (at least it was for me!), but as others have said, episode 3 is even more exciting and fast-paced - although it's still as wordy as the rest of umineko!
-              i read your comments about who you think the culprit(s) might be, but i want to ask what you think about the mysteries you've seen so far. do you think the murders were possible without magic? have you had a go at solving the epitaph?
+        text="""<p>yay! love reading people's opinions as they go through :) i think episode 2 is the one that really hooks you in and holds on (at least it was for me!), but as others have said, episode 3 is even more exciting and fast-paced - although it's still as wordy as the rest of umineko! i read your comments about who you think the culprit(s) might be, but i want to ask what you think about the mysteries you've seen so far. do you think the murders were possible without magic? have you had a go at solving the epitaph?</p><p>&nbsp;</p><p><br>&nbsp;</p>
               """,
         user=user3,
         thread=thread2
@@ -63,10 +50,7 @@ def seed_posts():
 
     post5 = Post(
         subject="RE: Umineko - I figured it out. (first time)",
-        text="""Good!
-        But. I assume you've played Higurashi, and know that Okonogi is not a guy to be taken up as a beacon of wholesome moral integrity. Nevertheless, he is the one who repeats this thing about love most throughout the story. Can you add this up somehow in your theory?
-
-        I think you'll very much enjoy Ep7 opening, all in all.""",
+        text="""<p>Good! But. I assume you've played Higurashi, and know that Okonogi is not a guy to be taken up as a beacon of wholesome moral integrity. Nevertheless, he is the one who repeats this thing about love most throughout the story. Can you add this up somehow in your theory? I think you'll very much enjoy Ep7 opening, all in all.</p><p>&nbsp;</p><p><br>&nbsp;</p>""",
         user=user2,
         thread=thread3
     )
@@ -75,8 +59,7 @@ def seed_posts():
 
     post6 = Post(
         subject="RE: Episode 5 completed! Here are my theories and attempts at solving the mystery!",
-        text="""Great writeup! You say that Battler never sees Shannon and Kanon in the same room, but there is a scene at the beginning of EP 5 where everyone on the island is present, how does that work?
-            Episode 6 gives away quite a lot of the mystery, so I'd advice you to do your attempts at answering the other questions sooner rather than later.
+        text="""<p>Great writeup! You say that Battler never sees Shannon and Kanon in the same room, but there is a scene at the beginning of EP 5 where everyone on the island is present, how does that work? Episode 6 gives away quite a lot of the mystery, so I'd advice you to do your attempts at answering the other questions sooner rather than later.</p><p>&nbsp;</p><p><br>&nbsp;</p>
             """,
         user=user2,
         thread=thread4,
@@ -86,8 +69,7 @@ def seed_posts():
 
     post7 = Post(
         subject="RE: Umineko master theory based on Question arcs (Spoilers for parts 1-4)",
-        text="""In Episode 1, Krauss dies on the first twilight. Do you think that Natsuhi would be an accomplice in a plan involving her husband's death? What about Episode 2 where they both die in the first twilight?
-                In Episode 3, how do you explain Nanjo's death? Shannon, Kanon, Nanjo, Krauss, Natsuhi, and Maria are all exempted via red truth.
+        text="""<p>In Episode 1, Krauss dies on the first twilight. Do you think that Natsuhi would be an accomplice in a plan involving her husband's death? What about Episode 2 where they both die in the first twilight? In Episode 3, how do you explain Nanjo's death? Shannon, Kanon, Nanjo, Krauss, Natsuhi, and Maria are all exempted via red truth.</p><p>&nbsp;</p><p><br>&nbsp;</p>
         """,
         user=user3,
         thread=thread5
--- app/seeds/lit_posts.py
+++ app/seeds/lit_posts.py
@@ -14,21 +14,7 @@ def seed_lit_posts():
 
     post1= Post(
         subject="RE: A first time reader's thoughts on the metaphysics of House of Leaves",
-        text="""Nice read, but the whole "it's just a work of fiction" argument falls short, as MZD makes himself part of his own fiction. All his books are part of a met-universe. E.G. VEM is present in all of his works.
-
-                But to take your approach of "So you make the descent, again" to the next level:
-
-                HoL explecitely mentiones Douglas Hofstadter in a fake interview, who is the auther of "I am a strange loop". His main idea is that self-consciousness arises from self-reference.
-
-                HoL contains itself and it refers to itself
-
-                HoL is a labyrinth without a minotaur - until you start reading. The reader becomes the Minotaur as he starts to inhabit the house
-
-                HoL gains consciousness in the moment where a reader starts to inhabit it - your mind is the "software" on which it's self-referntiality can play out
-
-                HoL is a kind of mental parasite that keeps you engaged with it and makes you to pass it on
-
-                And that's why it's so scary.
+        text="""<p>Nice read, but the whole "it's just a work of fiction" argument falls short, as MZD makes himself part of his own fiction. All his books are part of a met-universe. E.G. VEM is present in all of his works. But to take your approach of "So you make the descent, again" to the next level: HoL explecitely mentiones Douglas Hofstadter in a fake interview, who is the auther of "I am a strange loop". His main idea is that self-consciousness arises from self-reference. HoL contains itself and it refers to itself HoL is a labyrinth without a minotaur - until you start reading. The reader becomes the Minotaur as he starts to inhabit the house HoL gains consciousness in the moment where a reader starts to inhabit it - your mind is the "software" on which it's self-referntiality can play out HoL is a kind of mental parasite that keeps you engaged with it and makes you to pass it on And that's why it's so scary.</p><p>&nbsp;</p><p><br>&nbsp;</p>
               """,
         user=user2,
         thread=thread1,
@@ -37,11 +23,7 @@ def seed_lit_posts():
 
     post2 = Post(
         subject="RE: A first time reader's thoughts on the metaphysics of House of Leaves",
-        text="""Great post. Such a clear and concise overview of what I believe is the real strength of the book: it’s ability to subsume the reader into the narrative. I’d like to add, if I may, some additional thoughts on why this works so well.
-
-              The book is about trauma and how people overcome them (or don’t). Every character in the book grapples with some sort of regret or mistake or long suppressed pain from their past, and the House allows them to confront that issue head on. In the same way, by inviting the reader into the story, allowing them to shape the story as they perceive it, they too confront their own traumas or regrets. So many of the positive reviews for the book mention how it stuck with them or helped shape them in some way; that’s the power of the house and the book. The book’s themes allow it to echo into the reader’s life, which explains and justifies the meta nature of the book as a reflection of that reality. Ultimately, the answers to those unanswered questions can’t be found in the book, and that’s the point; only in reality do we know who wrote the book, Mark Z Danielewski. And only he has the ultimate answers, but each reader comes to terms with not knowing everything on their own and finds their own answers. Each reader confronts their Minotaur on their own. Each reader confronts their traumas and regrets on their own, often when feeling lost and confused, unable to find a way out, searching for answers that don’t exist and never will, stuck in a labyrinth of their own making.
-
-              Of course, that’s just my interpretation. Anything anyone gets out of the book is also valid. :)""",
+        text="""<p>Great post. Such a clear and concise overview of what I believe is the real strength of the book: it’s ability to subsume the reader into the narrative. I’d like to add, if I may, some additional thoughts on why this works so well. The book is about trauma and how people overcome them (or don’t). Every character in the book grapples with some sort of regret or mistake or long suppressed pain from their past, and the House allows them to confront that issue head on. In the same way, by inviting the reader into the story, allowing them to shape the story as they perceive it, they too confront their own traumas or regrets. So many of the positive reviews for the book mention how it stuck with them or helped shape them in some way; that’s the power of the house and the book. The book’s themes allow it to echo into the reader’s life, which explains and justifies the meta nature of the book as a reflection of that reality. Ultimately, the answers to those unanswered questions can’t be found in the book, and that’s the point; only in reality do we know who wrote the book, Mark Z Danielewski. And only he has the ultimate answers, but each reader comes to terms with not knowing everything on their own and finds their own answers. Each reader confronts their Minotaur on their own. Each reader confronts their traumas and regrets on their own, often when feeling lost and confused, unable to find a way out, searching for answers that don’t exist and never will, stuck in a labyrinth of their own making. Of course, that’s just my interpretation. Anything anyone gets out of the book is also valid. :)</p>""",
         user=user3,
         thread=thread1,
     )
@@ -49,7 +31,7 @@ def seed_lit_posts():
 
     post3= Post(
         subject="RE: House of Leaves - I believe I've found the profound message at the heart of this book. I might also be insane. Who knows.",
-        text="""I wholeheartedly agree. House of Leaves is predominantly about grief, and the desire to understand or find logic or reason in things that often reject understanding. You can try to understand or break down why someone passed away, just like you can go on countless expeditions to hopefully find the center of a maze, but in the end it will lead you right back to where you started. So much of this book is about letting go, as well as rejecting the notion that closure is a promise.""",
+        text="""<p>I wholeheartedly agree. House of Leaves is predominantly about grief, and the desire to understand or find logic or reason in things that often reject understanding. You can try to understand or break down why someone passed away, just like you can go on countless expeditions to hopefully find the center of a maze, but in the end it will lead you right back to where you started. So much of this book is about letting go, as well as rejecting the notion that closure is a promise.</p><p><br>&nbsp;</p>""",
         user=user2,
         thread=thread2,
     )
@@ -58,7 +40,7 @@ def seed_lit_posts():
 
     post4= Post(
         subject="RE: House of Leaves - I believe I've found the profound message at the heart of this book. I might also be insane. Who knows.",
-        text="""I agree! It's not about literally ignoring Johnny or any part of the book. It's realizing that none of our unreliable guides can lead us through the labyrinth, because they are themselves lost within it. But by the time you realize the book is playing with these concepts, you are lost in it yourself, seeking answers blindly, just like the characters are. I think that bewildering experience is kinda the point.
+        text="""<p>I agree! It's not about literally ignoring Johnny or any part of the book. It's realizing that none of our unreliable guides can lead us through the labyrinth, because they are themselves lost within it. But by the time you realize the book is playing with these concepts, you are lost in it yourself, seeking answers blindly, just like the characters are. I think that bewildering experience is kinda the point.</p><p>&nbsp;</p><p><br>&nbsp;</p>
               """,
         user=user3,
         thread=thread2,
@@ -68,11 +50,7 @@ def seed_lit_posts():
 
     post5 = Post(
         subject="RE:House of Leaves - The Author - A Unifying Theory",
-        text="""Great post, I agree. Adding to this: „A novel“ on the book cover is printed in purple, hinting that the whole book is the work of The Author.
-
-                It‘s pretty likely that The Author is Pelafina.
-
-                As good as the theory is when you only consider HoL, it basically gets impossible when you consider the screenplays which where released around 2 years ago. In these, TNR is revealed to be a real film. On the other hand, I‘m not sure what to make out of this, as it could just be a move from MZD to make things even more impossible to interpret coherently by intention.""",
+        text="""<p>Great post, I agree. Adding to this: „A novel“ on the book cover is printed in purple, hinting that the whole book is the work of The Author. It‘s pretty likely that The Author is Pelafina. As good as the theory is when you only consider HoL, it basically gets impossible when you consider the screenplays which where released around 2 years ago. In these, TNR is revealed to be a real film. On the other hand, I‘m not sure what to make out of this, as it could just be a move from MZD to make things even more impossible to interpret coherently by intention.</p><p>&nbsp;</p><p>&nbsp;</p>""",
         user=user2,
         thread=thread3,
     )
@@ -81,7 +59,7 @@ def seed_lit_posts():
 
     post6 = Post(
         subject="RE: Creepy personal connections in HOL",
-        text="""Interesting take. I went the completely other way and thought that maybe Johnny didn’t exist... and the whole thing was written by Pelafina. But it’s been a couple years since I’ve read it so I’m trying to remember exactly why I came to that conclusion. I think it was because there’s a line in there somewhere about Pelafina and Zampano knowing each other, which obviously shouldn’t be possible if the book is taken at face value. So I thought she created all of Johnny’s world in the throes of her mental illness and grief of the loss of her child. But I also think that is possible to come to a completely different conclusion every time a person reads it.
+        text="""<p>Interesting take. I went the completely other way and thought that maybe Johnny didn’t exist... and the whole thing was written by Pelafina. But it’s been a couple years since I’ve read it so I’m trying to remember exactly why I came to that conclusion. I think it was because there’s a line in there somewhere about Pelafina and Zampano knowing each other, which obviously shouldn’t be possible if the book is taken at face value. So I thought she created all of Johnny’s world in the throes of her mental illness and grief of the loss of her child. But I also think that is possible to come to a completely different conclusion every time a person reads it.</p><p>&nbsp;</p><p><br>&nbsp;</p>
             """,
         user=user2,
         thread=thread5,
@@ -91,8 +69,7 @@ def seed_lit_posts():
 
     post7 = Post(
         subject="RE: House of Leaves - Zampano's scratched floor",
-        text="""This is a great question, and one which I hadn’t thought. I like the red herring idea, but here’s the first thing that popped into my head, it could be total nonsense, but I thought, “Never Ending Story,” or a book that comes to life.
-        I think there might be points in the text to support this. I just finished reading HoL for the 4th time, but the first time in over a decade, and the first time since I got my English Degree. I’m going to go back through my annotated copy with this in mind. Thanks for the new inspiration. This book is so wildly dense, and I love this sub for posts like this.
+        text="""<p>This is a great question, and one which I hadn’t thought. I like the red herring idea, but here’s the first thing that popped into my head, it could be total nonsense, but I thought, “Never Ending Story,” or a book that comes to life. I think there might be points in the text to support this. I just finished reading HoL for the 4th time, but the first time in over a decade, and the first time since I got my English Degree. I’m going to go back through my annotated copy with this in mind. Thanks for the new inspiration. This book is so wildly dense, and I love this sub for posts like this.</p><p>&nbsp;</p><p><br>&nbsp;</p>
         """,
         user=user3,
         thread=thread5,
